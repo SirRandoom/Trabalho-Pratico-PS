1c2287f88e0881b31297c7f57befc4237c53c932
RafaelKoji <rafaelkvb@gmail.com>
2016-04-14 17:12:04 -0300

    aaa q raiva

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "pecas.h"
+
+int main(){
+	inicia_ncurses();
+	Tela* tela = cria_tela();
+	mostra_tela(tela);
+	
+	char get = getch();
+	if(pega_input(get)){
+		tela->estado = JOGO;
+	}
+	
+	nova_peca(tela);
+	mostra_tela(tela);
+		
+	while(pega_input(get)){
+		get=getch();
+		if(pega_input(get) == 2){
+			move_peca_y(tela->peca,1);
+			mostra_tela(tela);
+		}
+		if(pega_input(get) == 3){
+			move_peca_x(tela->peca,1);
+			mostra_tela(tela);
+		}
+		if(pega_input(get) == 4){
+			move_peca_x(tela->peca,-1);
+			mostra_tela(tela);
+		}
+	}	
+			
+	
+	destroi_tela(tela);
+	finaliza_ncurses();
+
+	return 0;
+	
+}
