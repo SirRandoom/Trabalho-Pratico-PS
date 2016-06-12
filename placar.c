e25553e73f221b7b171e3655f63ad0f29bc4703b
Luis Felipe <luisfelipe5417@gmail.com>
2016-06-12 20:25:32 -0300

    Data Placar

--- placar.c
+++ placar.c
@@ -2,6 +2,7 @@
 
 static placar *score;
 
+
 /**Função responsável pela criação do placar, abrindo o arquivo pontuacao.txt, ou criando-o caso não exista, e carregando suas informações na variável score do módulo*/
 void cria_placar(){
 	score = malloc(sizeof(placar));
@@ -15,27 +16,34 @@ void cria_placar(){
 	else{
 		int c;
 		int caracteres = 0, tabulacoes = 0;
-		int min = 0, sec = 0;
-		char* pontuacao = NULL, *tempo_m = NULL, *tempo_s = NULL;
-		char* nova_pontuacao = NULL, *novo_tempo_m = NULL, *novo_tempo_s = NULL;
+		int min = 0, sec = 0, data = 0;
+		char* pontuacao = NULL, *tempo_m = NULL, *tempo_s = NULL, *dia = NULL, *mes = NULL, *ano = NULL;
+		char* nova_pontuacao = NULL, *novo_tempo_m = NULL, *novo_tempo_s = NULL, *novo_dia = NULL, *novo_mes = NULL, *novo_ano = NULL;
 		while((c = fgetc(score->arquivo)) != EOF){
 			if(c == '\n'){
-				fprintf(Debug,"%s\t",pontuacao);
-				fprintf(Debug,"%s\t",tempo_m);
-				fprintf(Debug,"%s\n",tempo_s);
+				fprintf(Debug,"%s\t%s-%s-%s/%s:%s\n",pontuacao,dia,mes,ano,tempo_m,tempo_s);
 				score->pontuacoes[score->contador_jogadores] = (int) strtol(pontuacao, NULL, 10);
 				score->tempos_m[score->contador_jogadores] = (int) strtol(tempo_m,NULL,10);
 				score->tempos_s[score->contador_jogadores] = (int) strtol(tempo_s,NULL,10);
+				score->anos[score->contador_jogadores] = (int) strtol(ano,NULL,10);
+				score->meses[score->contador_jogadores] = (int) strtol(mes,NULL,10);
+				score->dias[score->contador_jogadores] = (int) strtol(dia,NULL,10);
 				score->contador_jogadores++;
 				tabulacoes = 0;
 				caracteres = 0;
-				min = sec = 0;
+				min = sec = data = 0;
 				pontuacao = NULL;
 				tempo_m = NULL;
 				tempo_s = NULL;
+				ano = NULL;
+				mes = NULL;
+				dia = NULL;
 				nova_pontuacao = NULL;
 				novo_tempo_m = NULL;
 				novo_tempo_s = NULL;
+				novo_dia = NULL;
+				novo_mes = NULL;
+				novo_ano = NULL;
 				continue;
 			}
 			if(c == '\t'){
@@ -56,6 +64,11 @@ void cria_placar(){
 					}
 					break;
 				case 2:
+					if(c == '-'){
+							data++;
+							caracteres = 0;
+							continue;
+						}
 					if(c == '/'){
 						min = 1;
 						caracteres = 0;
@@ -81,6 +94,29 @@ void cria_placar(){
 							tempo_s[caracteres] = c;	
 						}
 					}
+					if(!min && !sec){
+						if(!data){
+							novo_dia = realloc(dia,(caracteres+1)*sizeof(char));
+							if(novo_dia != NULL){
+								dia = novo_dia;
+								dia[caracteres] = c;
+							}
+						}
+						if(data==1){
+							novo_mes = realloc(mes,(caracteres+1)*sizeof(char));
+							if(novo_mes != NULL){
+								mes = novo_mes;
+								mes[caracteres] = c;
+							}
+						}
+						if(data==2){
+							novo_ano = realloc(ano,(caracteres+1)*sizeof(char));
+							if(novo_ano != NULL){
+								ano = novo_ano;
+								ano[caracteres] = c;
+							}
+						}
+					}
 			}
 			
 			caracteres++;
@@ -89,10 +125,15 @@ void cria_placar(){
 		free(pontuacao);
 		free(tempo_m);
 		free(tempo_s);
+		free(ano);
+		free(mes);
+		free(dia);
 		free(nova_pontuacao);
 		free(novo_tempo_m);
 		free(novo_tempo_s);
-				
+		free(novo_dia);
+		free(novo_mes);
+		free(novo_ano);		
 	}
 	
 	fclose(score->arquivo);  
@@ -101,6 +142,8 @@ void cria_placar(){
 /** Função responsável por atualizar o placar, com ordenamento por critério de maior pontuação, excluindo o jogador de menor pontuação do placar caso o número de jogadores atinja o máximo de 5 jogadores*/
 void atualiza_placar(int pontuacao){
 	
+	time_t temp = time(NULL);
+	struct tm tempo_local = *localtime(&temp);
 	int i, indice = score->contador_jogadores;
 	for(i = score->contador_jogadores-1; i >= 0 ; i--){
 		if(pontuacao > score->pontuacoes[i]){
@@ -114,6 +157,9 @@ void atualiza_placar(int pontuacao){
 			score->pontuacoes[i+1] = score->pontuacoes[i];
 			score->tempos_m[i+1] = score->tempos_m[i];
 			score->tempos_s[i+1] = score->tempos_s[i];
+			score->anos[i+1] = score->anos[i];
+			score->meses[i+1] = score->meses[i];
+			score->dias[i+1] = score->dias[i];
 		}
 	}
 	
@@ -122,6 +168,9 @@ void atualiza_placar(int pontuacao){
 		score->pontuacoes[indice] = pontuacao;
 		score->tempos_m[indice] = score->tempo_m;
 		score->tempos_s[indice] = score->tempo_s;
+		score->anos[indice] = 1900 + tempo_local.tm_year;
+		score->meses[indice] = tempo_local.tm_mon+1;
+		score->dias[indice] = tempo_local.tm_mday;
 	}
 	
 	if(score->contador_jogadores != 5){
@@ -131,7 +180,7 @@ void atualiza_placar(int pontuacao){
 	score->arquivo = fopen("pontuacao.txt","w");
 	
 	for(i = 0; i < score->contador_jogadores; i++){
-		fprintf(score->arquivo,"%s\t%d\t/%d:%d\n",score->jogadores[i],score->pontuacoes[i],score->tempos_m[i],score->tempos_s[i]);
+		fprintf(score->arquivo,"%s\t%d\t%d-%d-%d/%d:%d\n",score->jogadores[i],score->pontuacoes[i],score->dias[i],score->meses[i],score->anos[i],score->tempos_m[i],score->tempos_s[i]);
 	}
 	
 	fclose(score->arquivo);
@@ -140,14 +189,14 @@ void atualiza_placar(int pontuacao){
 /** Função responsável por mostrar o placar ao final do jogo*/
 void mostra_placar(){
 	WINDOW *janela;
-	janela = newwin(score->contador_jogadores+2,30,8,25);
+	janela = newwin(score->contador_jogadores+2,35,8,25);
 	wbkgd(janela,COLOR_PAIR(3));
-	box(janela, ACS_VLINE, ACS_HLINE);
-	mvwprintw(janela,0,12,"Placar");
 	int i;
 	for(i = 0; i < score->contador_jogadores; i++){
-		mvwprintw(janela,i+1,1,"%s\t%d\t/%d:%d",score->jogadores[i],score->pontuacoes[i],score->tempos_m[i],score->tempos_s[i]);
+		mvwprintw(janela,i+1,1,"%s\t%d\t%d-%d-%d/%d:%d\n",score->jogadores[i],score->pontuacoes[i],score->dias[i],score->meses[i],score->anos[i],score->tempos_m[i],score->tempos_s[i]);
 	}
+	box(janela, ACS_VLINE, ACS_HLINE);
+	mvwprintw(janela,0,14,"Placar");
 	wrefresh(janela);
 }
 
--- placar.h
+++ placar.h
@@ -3,18 +3,25 @@
 #include"engine.h"
 #include"tela.h"
 #include<string.h>
+#include<time.h>
 
 typedef struct placar{
 	char jogadores[5][4]; //vetor de jogadores no placar
 	int pontuacoes[5]; //vetor de pontuação dos jogadores no placar
 	int tempos_m[5];//vetor de tempo em minutos dos jogadores no placar
 	int tempos_s[5];//vetor de tempo em segundos dos jogadores no placar
+	int anos[5]; //vetor de anos dos jogadores no placar
+	int meses[5]; //vetor de meses dos jogadores no placar
+	int dias[5]; //vetor de dias dos jogadores no placar
 	int contador_jogadores; //contador de jogadores presentes no placar (máximo 5)
 	FILE *arquivo; //arquivo usado para abrir o placar (pontuacao.txt) para leitura e escrita
 	char jogador[3]; //nome do jogador atual
 	int pontuacao; //pontuacao do jogador atual
 	int tempo_m; //tempo em minutos do jogador atual
 	int tempo_s; //tempo em segundos do jogador atual
+	int ano; //ano do jogador atual
+	int mes; //mes do jogador atual
+	int dia; //dia do jogador atual
 }placar;
 
 
